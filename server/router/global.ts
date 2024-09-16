a18740d2b0381a6a16b48b88d64d4b22e693b341
Innei <i@innei.in>
2024-09-16 13:16:10 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- server/router/global.ts
+++ server/router/global.ts
@@ -3,7 +3,8 @@ import { createRequire } from "node:module"
 import path, { dirname, resolve } from "node:path"
 import { fileURLToPath } from "node:url"
 
-// import { getViteServer } from "../lib/dev-vite"
+import type { FastifyRequest } from "fastify"
+
 import { injectMetaHandler } from "../lib/meta-handler"
 
 const require = createRequire(import.meta.url)
@@ -20,24 +21,7 @@ const devHandler = (app: App) => {
 
       template = await vite.transformIndexHtml(url, template)
 
-      const dynamicInjectMetaString = await injectMetaHandler(url, req).catch((err) => {
-        if (process.env.NODE_ENV === "development") {
-          throw err
-        }
-        return ""
-      })
-      template = template.replace(`<!-- SSG-META -->`, dynamicInjectMetaString)
-
-      if (dynamicInjectMetaString) {
-        template = template.replace(`<!-- SSG-META -->`, dynamicInjectMetaString)
-
-        // Remove <!-- Default Open Graph --> between <!-- End Default Open Graph -->
-        const endCommentString = "<!-- End Default Open Graph -->"
-        const startIndex = template.indexOf("<!-- Default Open Graph -->")
-        const endIndex = template.indexOf(endCommentString)
-        template =
-          template.slice(0, startIndex) + template.slice(endIndex + endCommentString.length)
-      }
+      template = await transfromTemplate(template, req.originalUrl, req)
 
       reply.type("text/html")
       reply.send(template)
@@ -49,8 +33,34 @@ const devHandler = (app: App) => {
 }
 const handler = (app: App) => {
   app.get("*", async (req, reply) => {
-    // let template = readFileSync(path.resolve(root, vite.config.root, "index.html"), "utf-8")
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    let template = readFileSync(path.resolve(__dirname, "../web/index.html"), "utf-8")
+    template = await transfromTemplate(template, req.originalUrl, req)
+    reply.type("text/html")
+    reply.send(template)
   })
 }
 
 export const globalRoute = process.env.NODE_ENV === "development" ? devHandler : handler
+
+async function transfromTemplate(template: string, url: string, req: FastifyRequest) {
+  const dynamicInjectMetaString = await injectMetaHandler(url, req).catch((err) => {
+    if (process.env.NODE_ENV === "development") {
+      throw err
+    }
+    return ""
+  })
+  template = template.replace(`<!-- SSG-META -->`, dynamicInjectMetaString)
+
+  if (dynamicInjectMetaString) {
+    template = template.replace(`<!-- SSG-META -->`, dynamicInjectMetaString)
+
+    // Remove <!-- Default Open Graph --> between <!-- End Default Open Graph -->
+    const endCommentString = "<!-- End Default Open Graph -->"
+    const startIndex = template.indexOf("<!-- Default Open Graph -->")
+    const endIndex = template.indexOf(endCommentString)
+    template = template.slice(0, startIndex) + template.slice(endIndex + endCommentString.length)
+  }
+
+  return template
+}
